faaf8438fbca6cb3d1ed6d4470de7121256b2b96
Sasaki <redskirt@outlook.com>
2018-03-07 12:07:10 +0800

    computing product's support.

--- ml-advance/src/main/scala/com/sasaki/mla/main/Case01.scala
+++ ml-advance/src/main/scala/com/sasaki/mla/main/Case01.scala
@@ -29,16 +29,77 @@ object Case01 extends SparkHandler with ReflectHandler {
   def main(args: Array[String]): Unit = {
     
     invokeSessionHandler { () =>
+      // 0:date, 1:user_id, 2:package_name
       val rdd = spark.read.textFile(path).rdd.map(_.split('\t'))
-//      val df = rdd.toDF("date", "user_id", "package_name")
       
       println("Row Count: " + rdd.count)
-      println("User Count: " + rdd.map(_(1)).distinct.count)
-      println("Date Count: " + rdd.map(_(2)).distinct.count)
+      val userCount = rdd.map(_(1)).distinct.count
+      println("User Count: " + userCount)
+      println("Date Count: " + rdd.map(_(0)).distinct.count)
       
+      /**
+       * 指定任意1天的数据，根据每个用户的安装列表，统计每个包名的安装用户数量，
+       * 由大到小排序，并且取前1000个包名，最后计算这1000个包名之间的支持度和置信度。
+       */
+      val filterDate = "2016-03-29"
+      val rddDate = rdd.filter(_(0) == filterDate)
+      val rddUser___installPackages = rddDate.map(r => (r(1), r(2)))
+        .groupByKey()
+        
+      val rddInstallCount___package_name = rddDate.map(r => (r(2), r(1)))
+        .groupByKey()
+        .map { case(p, u) => (u.toSet.size -> p)}
+        .sortByKey(false)
+      /**
+       * (995,com.jb.emoji.gokeyboard)
+       * (678,com.facebook.orca)
+       * (585,com.facebook.katana)
+       * (385,com.instagram.android)
+       */
       
-      
-
+        /**
+         * 分析求支持度/Support 和 置信度/Confidence 问题
+         * 
+         * 用户user/4
+         * 1 2 3 4
+         * 
+         * 商品product/5
+         * a b c d e
+         * 
+         * 每个用户的商品购买清单：
+         * 1 : a b c
+         * 2 : a d
+         * 3 : c e
+         * 4 : a d e
+         * 
+         * --> 分别求5种商品两两组合(p1, p2)在购买清单中的命中数
+         * 
+         * 
+         * 
+         * a : 3
+         * b : 10
+         * c : 4
+         * d : 6
+         * e : 8
+         * 
+         */
+        
+                
+      val rddPackage1000 = spark.sparkContext.parallelize(rddInstallCount___package_name.map(_._2).take(1000))
+      val rddIndex___Package1000 = rddPackage1000.zipWithIndex().map(o => (o._2, o._1))
+      // 实现RDD元素的二维组合算法
+      val rddCombinePackage1000 = rddIndex___Package1000.cartesian(rddIndex___Package1000)
+        .filter(o => o._1._1 < o._2._1 )
+        .map{ case(o, o_) => (o._2 -> o_._2)}
+      rddCombinePackage1000 take(10) foreach println
+      val t = rddCombinePackage1000.map { case(o, o_) =>
+        val user = rddUser___installPackages.filter { case (u, ps) =>
+          ps.exists(p => p == o && p == o_)
+        }
+        .map(_._1)
+        .first
+        (s"$o_$o_" -> user)
+      }
     }
   }
 }
--- ml-advance/src/main/scala/com/sasaki/mla/sample/Test.scala
+++ ml-advance/src/main/scala/com/sasaki/mla/sample/Test.scala
@@ -1,7 +1,28 @@
 package com.sasaki.mla.sample
 
-object Test {
+import com.sasaki.spark.SparkHandler
+
+object Test extends SparkHandler {
+  
+   lazy val spark = buildLocalSparkSession(false)
+  implicit val _spark_ = spark
+  
   def main(args: Array[String]): Unit = {
-    println(11)
+    
+    // 实现RDD元素二维组合算法，可以考虑用笛卡尔积加索引方式和迭代zip方式
+     /**
+         * a b c d e 
+         *   a b c d e 
+         *     a b c d e 
+         */
+    
+     invokeSessionHandler { () =>
+       val rdd = spark.sparkContext.parallelize(List("a", "b", "c", "d", "e"))
+       val rddIndex___Item = rdd.zipWithIndex().map(o => (o._2, o._1))
+       rddIndex___Item.cartesian(rddIndex___Item).filter(o => o._1._1 < o._2._1 )
+         .map{ case(o, o_) => (o._2 -> o_._2 )}  foreach println
+       
+     }
   }
+   
 }
